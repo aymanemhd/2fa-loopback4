8b78ef130de4bb7a3e0472e9a20d792a1916af40
aymanemhd <aymane.199820.mehdi@gmail.com>
2023-04-03 07:26:26 +0200

    add userdb model

---
+++ src/models/userdb.model.ts
@@ -0,0 +1,27 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Userdb extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Userdb>) {
+    super(data);
+  }
+}
+
+export interface UserdbRelations {
+  // describe navigational properties here
+}
+
+export type UserdbWithRelations = Userdb & UserdbRelations;
